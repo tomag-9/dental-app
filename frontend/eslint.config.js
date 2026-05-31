0ba35ecb28a8b300f3128a3a417f8a7c2318a335
magi-9 <dev@tomag.xyz>
2026-05-31 11:01:10 +0300

    Restructure frontend source layout

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -11,6 +11,11 @@ export default [
     languageOptions: {
       ecmaVersion: 2023,
       sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
       globals: {
         ...globals.browser,
         React: 'readonly',
